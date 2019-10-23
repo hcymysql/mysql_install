e8f645513fe10b7c71d11b308c6658131132410d
hcymysql <chunyang_he@139.com>
2019-10-23 17:51:09 +0800

    增加clone克隆插件安装
    增加clone克隆插件安装

--- mysql8_install.sh
+++ mysql8_install.sh
@@ -4,11 +4,35 @@ echo "正在安装MySQL软件......."
 
 useradd mysql
 useradd nagios
+useradd zabbix
 
 sleep 2
+
+######配置参数######
 mysql8_version=mysql-8.0.18-linux-glibc2.12-x86_64.tar.xz
 mysql8_version_dir=mysql-8.0.18-linux-glibc2.12-x86_64
 
+######同步复制用户######
+repl_user=repl
+repl_passwd=sysrepl
+#######################
+
+######root密码######
+root_passwd=123456
+#######################
+
+######DBA管理用户######
+dba_user=admin
+dba_passwd=hechunyang
+#######################
+
+######8.0克隆用户######
+clone_user=clone_user
+clone_passwd=123456
+#######################
+
+
+######修改hosts文件######
 cat << EOF >> /etc/hosts
 
 192.168.137.11		mgr1
@@ -16,15 +40,15 @@ cat << EOF >> /etc/hosts
 192.168.137.13		mgr3
 
 EOF
-
+#######################
 
 ######mgr配置######
 mysql_port=3306
-master_ip=192.168.137.11
+primary_ip=192.168.137.11
 secondary1_ip=192.168.137.12
 secondary2_ip=192.168.137.13
 
-master_port=33061
+primary_port=33061
 secondary1_port=33062
 secondary2_port=33063
 
@@ -49,7 +73,7 @@ do
 	fi
 done
 
-/usr/local/mysql/bin/mysql -h127.0.0.1 -u'admin' -p'hechunyang' -P"$master_port" --connect-expired-password -e "CHANGE MASTER TO MASTER_HOST='$master_ip',MASTER_USER='repl',MASTER_PASSWORD='sysrepl',MASTER_PORT=$master_port,MASTER_AUTO_POSITION = 1,MASTER_CONNECT_RETRY=10; START SLAVE;"
+/usr/local/mysql/bin/mysql -h127.0.0.1 -u'$dba_user' -p'$dba_passwd' -P"$master_port" --connect-expired-password -e "CHANGE MASTER TO MASTER_HOST='$master_ip',MASTER_USER='repl',MASTER_PASSWORD='sysrepl',MASTER_PORT=$master_port,MASTER_AUTO_POSITION = 1,MASTER_CONNECT_RETRY=10; START SLAVE;"
 
 	echo "MySQL主从复制同步已经初始化完毕。"
 	exit 0
@@ -78,10 +102,10 @@ done
 
 if [ $is_primary == "yes" ]
 then
-	/usr/local/mysql/bin/mysql -h127.0.0.1 -u'admin' -p'hechunyang' -P"$mysql_port" --connect-expired-password -e "INSTALL PLUGIN group_replication SONAME  'group_replication.so'; set persist group_replication_group_name = '3b12b5bd-f0c6-11e9-9778-000c2900afc6';set persist group_replication_local_address =  '${local_ip}:${local_port}'; set persist group_replication_group_seeds = '${master_ip}:${master_port},${secondary1_ip}:${secondary1_port},${secondary2_ip}:${secondary2_port}';SET GLOBAL group_replication_bootstrap_group=ON; CHANGE MASTER TO MASTER_USER='repl',MASTER_PASSWORD='sysrepl' FOR CHANNEL 'group_replication_recovery';START GROUP_REPLICATION;select sleep(5);select * from performance_schema.replication_group_members;SET GLOBAL group_replication_bootstrap_group=OFF;"
+	/usr/local/mysql/bin/mysql -h127.0.0.1 -u"$dba_user" -p"$dba_passwd" -P"$mysql_port" --connect-expired-password -e "INSTALL PLUGIN group_replication SONAME  'group_replication.so'; set persist group_replication_group_name = '3b12b5bd-f0c6-11e9-9778-000c2900afc6';set persist group_replication_local_address =  '${local_ip}:${local_port}'; set persist group_replication_group_seeds = '${primary_ip}:${primary_port},${secondary1_ip}:${secondary1_port},${secondary2_ip}:${secondary2_port}';SET GLOBAL group_replication_bootstrap_group=ON; CHANGE MASTER TO MASTER_USER='$repl_user',MASTER_PASSWORD='$repl_passwd' FOR CHANNEL 'group_replication_recovery';START GROUP_REPLICATION;select sleep(5);select * from performance_schema.replication_group_members;SET GLOBAL group_replication_bootstrap_group=OFF;"
 
 else
-	/usr/local/mysql/bin/mysql -h127.0.0.1 -u'admin' -p'hechunyang' -P"$mysql_port" --connect-expired-password -e "INSTALL PLUGIN group_replication SONAME  'group_replication.so'; set persist group_replication_group_name = '3b12b5bd-f0c6-11e9-9778-000c2900afc6';set persist group_replication_local_address =  '${local_ip}:${local_port}'; set persist group_replication_group_seeds = '${master_ip}:${master_port},${secondary1_ip}:${secondary1_port},${secondary2_ip}:${secondary2_port}'; SET GLOBAL group_replication_bootstrap_group=OFF; CHANGE MASTER TO MASTER_USER='repl',MASTER_PASSWORD='sysrepl' FOR CHANNEL 'group_replication_recovery';START GROUP_REPLICATION;select sleep(5);select * from performance_schema.replication_group_members;"; 
+	/usr/local/mysql/bin/mysql -h127.0.0.1 -u"$dba_user" -p"$dba_passwd" -P"$mysql_port" --connect-expired-password -e "INSTALL PLUGIN group_replication SONAME  'group_replication.so'; set persist group_replication_group_name = '3b12b5bd-f0c6-11e9-9778-000c2900afc6';set persist group_replication_local_address =  '${local_ip}:${local_port}'; set persist group_replication_group_seeds = '${primary_ip}:${primary_port},${secondary1_ip}:${secondary1_port},${secondary2_ip}:${secondary2_port}'; SET GLOBAL group_replication_bootstrap_group=OFF; CHANGE MASTER TO MASTER_USER='$repl_user',MASTER_PASSWORD='$repl_passwd' FOR CHANNEL 'group_replication_recovery';START GROUP_REPLICATION;select sleep(5);select * from performance_schema.replication_group_members;"
 
 fi
 	
@@ -170,12 +194,12 @@ fi
 
 ###更改root账号随机密码
 random_passwd=`grep 'temporary password' $DATA_DIR/log/error.log | awk -F 'root@localhost: ' '{print $2}'`
-/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock -p"$random_passwd" --connect-expired-password -e "set sql_log_bin=0;alter user root@'localhost' identified by '123456';" 
+/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock -p"$random_passwd" --connect-expired-password -e "set sql_log_bin=0;alter user root@'localhost' identified by '$root_passwd';" 
 
 echo "root账号随机密码更改完毕。"
 
 ###创建同步账号和管理员账号
-/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock --connect-expired-password -p'123456' -e "set sql_log_bin=0;create user 'repl'@'%' IDENTIFIED BY 'sysrepl'; GRANT REPLICATION SLAVE,REPLICATION CLIENT ON *.* TO 'repl'@'%'; create user 'admin'@'%' IDENTIFIED BY 'hechunyang'; GRANT ALL on *.* to 'admin'@'%' WITH GRANT OPTION;"
+/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock --connect-expired-password -p"$root_passwd" -e "set sql_log_bin=0;create user '$repl_user'@'%' IDENTIFIED BY '$repl_passwd'; GRANT REPLICATION SLAVE,REPLICATION CLIENT ON *.* TO '$repl_user'@'%'; create user '$dba_user'@'%' IDENTIFIED BY '$dba_passwd'; GRANT ALL on *.* to '$dba_user'@'%' WITH GRANT OPTION;"
 
 sed -i -r "s/(PATH=)/\1\/usr\/local\/mysql\/bin:/" /root/.bash_profile
 source /root/.bash_profile
@@ -183,9 +207,8 @@ source /root/.bash_profile
 echo "MySQL账号初始化完毕。"
 
 ###安装clone插件
-/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock --connect-expired-password -p'123456' -e "set sql_log_bin=0;INSTALL PLUGIN CLONE SONAME 'mysql_clone.so'; CREATE USER 'clone_user'@'%' IDENTIFIED BY '123456';GRANT BACKUP_ADMIN,CLONE_ADMIN ON *.* TO 'clone_user'@'%';"
+/usr/local/mysql/bin/mysql -S /tmp/mysql_$dbname.sock --connect-expired-password -p"$root_passwd" -e "set sql_log_bin=0;INSTALL PLUGIN CLONE SONAME 'mysql_clone.so'; CREATE USER '$clone_user'@'%' IDENTIFIED BY '$clone_passwd';GRANT BACKUP_ADMIN,CLONE_ADMIN ON *.* TO '$clone_user'@'%';"
 
 echo ""
 echo "clone克隆插件安装完毕。"
-
 
